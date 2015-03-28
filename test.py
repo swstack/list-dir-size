c58965be2010fb0e9d5932ad19edd57403ec5829
Stephen Stack <ss@stephenstack.com>
2015-03-28 00:42:05 -0500

    test: stub for tests

---
+++ test.py
@@ -0,0 +1,54 @@
+import unittest
+
+
+class TestListDirSizeHelpers(unittest.TestCase):
+    """Test all of the helper functions of `list_dir_size`"""
+
+    def setUp(self):
+        """Run at the start of each test"""
+
+    def tearDown(self):
+        """Run at the end of each test"""
+
+    def test_human_readable(self):
+        pass  # TODO
+
+    def test_calc_dir_size(self):
+        pass  # TODO
+
+    def test_walk_level(self):
+        pass  # TODO
+
+    def test_normalize_path(self):
+        pass  # TODO
+
+    def test_get_args(self):
+        pass  # TODO
+
+
+class TestListDirSizeCLIBlackBox(unittest.TestCase):
+    """This is a black box text suite against the `list_dir_size` CLI"""
+
+    def setUp(self):
+        """Run at the start of each test"""
+
+    def tearDown(self):
+        """Run at the end of each test"""
+
+    def test_path_cwd(self):
+        pass  # TODO
+
+    def test_path_include_tilde(self):
+        pass  # TODO
+
+    def test_path_complex(self):
+        pass  # TODO
+
+    def test_human_readable(self):
+        pass
+
+    def test_not_human_readable(self):
+        pass
+
+
+
